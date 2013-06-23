3751f90a5db6ac50fd259f2f332509cbd1e3b841
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-06-23 23:35:05 +0100

    How to make a delete button

--- app.rb
+++ app.rb
@@ -25,7 +25,14 @@ helpers do
    time.strftime("%d %b %Y")
   end
 
-end 
+  def post_show_page?
+    request.path_info =~ /\/posts\/\d+$/
+  end
+
+  def delete_post_button(post_id)
+    erb :_delete_post_button, locals: { post_id: post_id}
+  end
+end
 
 #################### routes
 
